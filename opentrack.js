cd8c1e537b957fcebdf0ca57276c540180527bd4
dragonstrong <32926995+dragonstrong@users.noreply.github.com>
2021-09-14 14:41:01 +0800

    Update opentrack.js

--- opentrack.js
+++ opentrack.js
@@ -2,4 +2,9 @@ const OpenTrack = require('opentrack')
 const client = new OpenTrack.Client('10.128.200.231', 4243)
 
 // Called every time data is sent
-
+client.onUpdate((transform, delta) => {
+	// The modified transform will be sent on the next update
+	transform.rotation.z += delta * 15
+	transform.rotation.x = Transform.fromBuffer(buffer).rotation.x
+	transform.rotation.y = Transform.fromBuffer(buffer).rotation.y
+})
